48db17ec98e8593acca48941572474eab75b5743
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2024-01-22 19:59:22 +0100

    persistGate

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -31,7 +31,7 @@ const contactsConfig = {
     },
 }),});
 
-export const persister = persistStore(store);
+export const persistor = persistStore(store);
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App/App';
 import { Provider } from "react-redux";
- import { store } from "./redux/store"
+import{ PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PersistGate persistor={persistor} loading={null}>
+     <Provider store={store}>
     <BrowserRouter basename="/goit-react-hw-06-phonebook">
-    <Provider store={store}>  
   <App />  
- </Provider>
     </BrowserRouter>
+    </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
